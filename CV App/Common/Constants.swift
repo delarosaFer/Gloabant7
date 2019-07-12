fa555358a9109075780e9780ec4642c89fc2cacd
Fernando De La Rosa <fernando.delarosa@globant.com>
2019-07-12 11:18:43 -0500

    remove all empty files

--- CV App/Common/Constants.swift
+++ CV App/Common/Constants.swift
@@ -1,6 +1,4 @@
-
 import Foundation
-
 import CoreGraphics
 
 // MARK: - Storyboard name
@@ -84,15 +82,7 @@ enum NetworkingErrors: Error{
 
 // MARK: - Accessibilit yIdentifiers
 enum AccessibilityIdentifiers: String {
-    case topFirstTitle = "topFirstTitle"
-    case topSecondTitle = "topSecondTitle"
-    case userNameLabel = "userNameLabel"
-    case ageLabel = "ageLabel"
-    case cellphoneLabel = "cellphoneLabel"
-    case emailLabel = "emailLabel"
-    case aboutMeLabel = "aboutMeLabel"
     case moreInfo = "moreInfoButton"
-    case profileUserImage = "profileUserImage"
     case myCareerNavBar = "myCareerNavigationBar"
 }
   
