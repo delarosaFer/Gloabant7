b10c07607a8d05ee2b99d771a3367ffefe8a9d08
Fernando De La Rosa <fernando.delarosa@globant.com>
2019-07-12 16:46:15 -0500

    add decode json test

--- CV AppTests/Common/Protocols.swift
+++ CV AppTests/Common/Protocols.swift
@@ -7,8 +7,12 @@ protocol CommonMethods {
 
 extension CommonMethods where Self: AnyObject {
     func getDataFrom(file: String) -> Data {
-        guard let path = Bundle(for: type(of: self)).url(forResource: file, withExtension: ExtensionFile.json.rawValue) else { return Data() }
-        guard let data  = try? Data(contentsOf: path) else { return Data() }
+        guard let path = Bundle(for: type(of: self)).url(forResource: file, withExtension: ExtensionFile.json.rawValue) else {
+            return Data()
+        }
+        guard let data  = try? Data(contentsOf: path) else {
+            return Data()
+        }
         return data
     }
 }
